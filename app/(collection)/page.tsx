8bdfab8a1f56336aa491c71630b8d476f17d4ffb
berkaycirak <berkaycirak_11@hotmail.com>
2024-07-14 12:04:25 +0300

    refactor:tabs are refactored

--- app/(collection)/page.tsx
+++ app/(collection)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import YourAssets from "./component/YourAssets";
 import ListedAssets from "./component/ListedAssets";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Lock } from "lucide-react";
 
 const CollectionPage = () => {
   return (
@@ -9,14 +10,18 @@ const CollectionPage = () => {
       {/* Banner For MarketHall  */}
       <div></div>
       {/* Tabs */}
-      <Tabs defaultValue="market" className="mx-auto w-[400px]">
-        <TabsList className="flex w-full items-center">
+      <Tabs defaultValue="market" className="mx-auto w-full">
+        <TabsList className="mx-auto mb-6 flex w-full items-center md:max-w-screen-md xl:mb-12 2xl:max-w-screen-lg">
           <TabsTrigger value="market">Market</TabsTrigger>
-          <TabsTrigger value="owned">Owned Rabbids</TabsTrigger>
+          <TabsTrigger value="owned">Owned Rabbidos</TabsTrigger>
+          <TabsTrigger value="mint" className="cursor-not-allowed" disabled>
+            <Lock size={14} /> Mint Rabiddo
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="market">
           <ListedAssets />
         </TabsContent>
+
         <TabsContent value="owned">
           <YourAssets />
         </TabsContent>
